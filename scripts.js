f570030d1174c9eaecb184a254a3d38ffd5b363f
JavSanchez97 <javsanchez2015@gmail.com>
2022-02-06 21:07:51 -0500

    Finished all changes. Should be working properly. Not very pretty though.

--- scripts.js
+++ scripts.js
@@ -5,37 +5,47 @@ const resetBtn = document.querySelector('.reset');
 const applyGridSize = document.querySelector('.apply');
 let squareSize = 16;
 
+createGrid(squareSize);
 
+//this will make the divs square
+function createDiv(size){
+  const div=document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+  return div;
+}
+//and this will make the actual grid and append to the HTML grid class
+function createGrid(gridSize){
+  for (let i = 0;i < gridSize; i++){
+    for(let j = 0; j < gridSize; j++){
+      grid.appendChild(createDiv(grid.clientWidth / gridSize));
+    }
+  }
+}
 
+function reset(){
+  while(grid.firstChild){
+    grid.removeChild(grid.lastChild);
+  }
+  createGrid(squareSize);
+}
+  
+grid.addEventListener('mouseover', function(e){
+  //only makes the divs that are boxes active
+  if (e.target.matches('.box')){
+    e.target.classList.add('active');
+  }
+});
 
+//this will change the size of the grid
+gridSize.addEventListener('input', function(e){
+  squareSize = e.target.value;
+  gridValue.textContent = `${squareSize}x${squareSize}`;
+});
 
+applyGridSize.addEventListener('click', function(){
+  reset();
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const container = document.getElementById("container");
-
-// function makeRows(rows, cols) {
-//   container.style.setProperty('--grid-rows', rows);
-//   container.style.setProperty('--grid-cols', cols);
-//   for (c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     //cell.innerText = (c + 1);
-//     container.appendChild(cell).className = "grid-item";
-//   };
-// };
-
-// makeRows(gridSize, gridSize);
-// //event listening for the mouse to hover over the grid
-// container.addEventListener('mouseover', function (e){
-
-// })
+resetBtn.addEventListener('click', reset);
