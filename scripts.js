bbf0bf6a336b2e901add8b86d47e00dc1de4bfcf
JavSanchez97 <javsanchez2015@gmail.com>
2022-02-06 19:19:48 -0500

    Testing new method. New HTML setup, setting up JS and CSS

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
-gridSize = 16;
-
-const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 16;
 
 
-makeRows(gridSize, gridSize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const container = document.getElementById("container");
+
+// function makeRows(rows, cols) {
+//   container.style.setProperty('--grid-rows', rows);
+//   container.style.setProperty('--grid-cols', cols);
+//   for (c = 0; c < (rows * cols); c++) {
+//     let cell = document.createElement("div");
+//     //cell.innerText = (c + 1);
+//     container.appendChild(cell).className = "grid-item";
+//   };
+// };
+
+// makeRows(gridSize, gridSize);
+// //event listening for the mouse to hover over the grid
+// container.addEventListener('mouseover', function (e){
+
+// })
